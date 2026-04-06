00718110b35be962cf8a20361566e6da4d18d481
raf1314 <rafael.cordova1314@gmail.com>
2026-04-06 13:47:39 +0000

    Agrego scripts EC2 y respaldo S3

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Sincronizando archivos con S3"
-aws s3 sync src/ s3://mi-bucket-devops-cn --delete --acl public-read
+aws s3 sync src/ s3://mi-bucket-devops-rcn --delete
 echo "Despliegue completado"
